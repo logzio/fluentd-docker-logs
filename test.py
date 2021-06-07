958ffc16c507f3079c494df17acfb9c3c9e25ab9
yotamloe <yotam12341@gmail.com>
2021-06-07 16:21:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ class TestFluentdIntegration(unittest.TestCase):
         log_count = int(json.loads(response.text)['hits']['total'])
         print(f'api_token: {api_token}')
         print(f'Query: {query}')
-        print(response.text)
+        print(response.request.body)
         valid_count = 50
         self.assertEqual(log_count, valid_count, f"Should be {valid_count}")
 
